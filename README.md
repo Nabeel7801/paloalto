828fc6e4b5e226b980ca17f3a2ec3cf2b6060d9f
Nabeel Ahmed <nabeel.ahmed0026@gmail.com>
2022-07-29 23:34:37 +0500

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # paloalto
 This project is a Replica of https://reg.ignite.paloaltonetworks.com/
+
+# To Run this Project
+ 1) Clone the Repository
+ 2) Install Node Module (npm install)
+ 3) And then Start the App (npm start)
+
+# Pre Requisites
+1) Node JS
+2) VS Code
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <React.Fragment>
+            <Header />
+            <Footer />
+        </React.Fragment>
+    );
+}
+
+export default App;
