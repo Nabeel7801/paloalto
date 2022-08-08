319765a6c73bbeee084283a89cb74a2be8f08fd1
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2022-08-08 21:27:50 +0100

    More JSON Implementated

--- client/src/pages/Session.jsx
+++ client/src/pages/Session.jsx
@@ -23,11 +23,119 @@ function Session() {
         { 
             title: "Cloud Native Application Protection (Prisma Cloud)",
             usecases: [
-                "Cloud Code Security (Infrastructure as Code Security) ",
-                
+                "Cloud Code Security (Infrastructure as Code Security)",
+                "Cloud Infrastructure Entitlement Management (CIEM)",
+                "Cloud Native Application Protection Platform (CNAPP)",
+                "Cloud Network Security and Microsegmentation",
+                "Cloud Security Posture Management (CSPM)",
+                "Cloud Workload Protection (CWP)",
+                "Web App and API Security"
             ]
         },
-        
+        { 
+            title: "Endpoint Security (Cortex XDR)",
+            usecases: [
+                "Digital Forensics",
+                "Endpoint Protection",
+                "Endpoint Detection &amp; Response",
+                "Ransomware Protection"
+
+            ]
+        },
+        { 
+            title: "Industry Solutions",
+            usecases: [
+                "Financial Services",
+                "Healthcare",
+                "ICS/SCADA",
+                "Manufacturing",
+                "Oil & Gas",
+                "Retail",
+                "Service Providers",
+                "State and local Government and Education",
+                "Utilities",
+                "Other"
+            ]
+        },
+        { 
+            title: "Network Security (Next-Gen Firewall)",
+            usecases: [
+                "AIOPs",
+                "Container Network Security / Container Firewalls",
+                "Data Center Security",
+                "Enterprise 5G Security",
+                "Network Perimeter / Internet Edge Security",
+                "Network Security Management",
+                "Public Cloud Network Security &amp; Cloud NGFW",
+                "Secure Network Segmentation",
+                "Securing SDN/Virtualized Infrastructure",
+                "Zero Trust for Users",
+                "Zero Trust Security"
+            ]
+        },
+        { 
+            title: "Partner Showcase",
+            usecases: [
+                "Actively Using",
+                "In-Deployment",
+                "Prospect Customer",
+                "Not Applicable"
+            ]
+        },
+        { 
+            title: "Secure Access Service Edge (SASE)",
+            usecases: [
+                "Autonomous Digital Experience Management (ADEM)",
+                "Cloud Access Security Broker (CASB)",
+                "Cloud Secure Web Gateway (SWG)",
+                "Firewall as a Service (FWaaS)",
+                "Home Network (Okyo Garde)",
+                "SD-WAN",
+                "Zero Trust Network Access (ZTNA)"
+            ]
+        },
+        { 
+            title: "Security Operations (Cortex)",
+            usecases: [
+                "Attack Surface Management",
+                "Cloud Security & Automation",
+                "Cloud Detection & Response",
+                "Compliance Management",
+                "Incident Case Management",
+                "Managed Detection & Response",
+                "Managed Threat Hunting",
+                "Network Security Automation",
+                "Security Operations Workflow Automation",
+                "Third-Party Security",
+                "Threat Intel Management",
+                "Umanaged Cloud Security"
+            ]
+        },
+        { 
+            title: "Simplifying Security (Cross Portfolio)",
+            usecases: [
+                "Complete Cloud Security",
+                "Consolidating Security with the PANW Portfolio",
+                "Hybrid Work",
+                "Securing 5G",
+                "Zero Trust Enterprise: A Deep Dive"
+            ]
+        },
+        { 
+            title: "Threat Intel & Incideent Response Services (Unit 42)",
+            usecases: [
+                "Advanced Persistent Threat Investigation",
+                "Breach Readiness",
+                "Business Email Compromise",
+                "Cloud Incident Response",
+                "Cybersecurity Risk Assessments",
+                "Incident Response",
+                "Incident Simulations (Tabletop Exercises, Red/Blue/Purple Team Exercises)",
+                "Ransomware Investigation",
+                "Threat Intelligence/Threat Landscape",
+                "Supply Chain Risk Assessments"
+            ]
+        },
     ]
 
     useEffect(() => {
