0874c1cff2b05f4325f644ac145ba9b56f809afa
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2022-11-03 23:54:49 +0100

    Fix cards img

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,4 +1,7 @@
-import bg from '../assets/img/speaker.jpg';
+import vtf from '../assets/img/vtf.png';
+import isaca from '../assets/img/isaca.png';
+import csa from '../assets/img/csa.png';
+import fuel from '../assets/img/fuel.png'
 
 
 function About() {
@@ -7,46 +10,58 @@ function About() {
     <div className="container pageContainer">
       <div className='row'>
         <div className='col'>
-          <div class="card" style={{width: "18rem"}}>
-            <img src={bg} class="card-img-top" alt="..."/>
+          <div class="card" style={{width: "17rem"}}>
+            <img src={vtf} class="card-img-top" alt="..."/>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             
             <div class="card-body">
-              <a href="/" class="card-link">Card link</a>
-              <a href="/" class="card-link">Another link</a>
+              <a href="/" class="card-link">Learn more</a>
             </div>
           </div>
         </div>
 
         <div className='col'>
-          <div class="card" style={{width: "18rem"}}>
-            <img src={bg} class="card-img-top" alt="..."/>
+          <div class="card" style={{width: "17rem"}}>
+            <img src={isaca} class="card-img-top" alt="..."/>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             
             <div class="card-body">
-              <a href="/" class="card-link">Card link</a>
-              <a href="/" class="card-link">Another link</a>
+              <a href="/" class="card-link">Learn more</a>
             </div>
           </div>
         </div>
 
         <div className='col'>
-          <div class="card" style={{width: "18rem"}}>
-            <img src={bg} class="card-img-top" alt="..."/>
+          <div class="card" style={{width: "17rem"}}>
+            <img src={csa} class="card-img-top" alt="..."/>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             
             <div class="card-body">
-              <a href="/" class="card-link">Card link</a>
-              <a href="/" class="card-link">Another link</a>
+              <a href="/" class="card-link">Learn more</a>
+            </div>
+          </div>
+        </div>
+
+        <div className='col'>
+          <div class="card" style={{width: "17rem"}}>
+            <img src={fuel} class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            
+            <div class="card-body">
+              <a href="/" class="card-link">Learn more</a>
+              
             </div>
           </div>
         </div>
